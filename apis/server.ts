9afbf5171ea91591a6d017bf3b97a1a4b3c301e7
pacifiquem <Murangwa Pacifique>
2023-10-24 16:57:24 +0200

    [init]: express basic setup

---
+++ apis/server.ts
@@ -0,0 +1,12 @@
+import http from 'http';
+import app from './src';
+import { config } from 'dotenv';
+config();
+
+const port:number = Number(process.env.PORT) || Number(3000);
+
+const server:http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running @ ${port}`);
+});
---
+++ apis/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import { StatusCodes } from "http-status-codes";
+import { UnSuccessfulApiResponse } from "./helper/ApiResponse.helper";
+import database_connection from "./helper/db.helper";
+
+database_connection();
+const app: Application = express();
+app.use(express.json()).use(cors()).use(morgan('tiny')).use(helmet());
+
+
+app.all("*", (req: Request, res: Response) => {
+    res.status(StatusCodes.NOT_FOUND).json(new UnSuccessfulApiResponse(false, "No such route on this server."));
+});
+
+export default app;
