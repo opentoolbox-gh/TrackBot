a40ef14c0b313b0b36b3cc5ab1b31bc46477a701
Hallcoder <hallcoder25@gmail.com>
2023-10-24 16:40:11 +0200

    firebase auth functions

--- web/src/firebase/firebaseConfig.ts
+++ web/src/firebase/firebaseConfig.ts
@@ -9,3 +9,28 @@ const firebaseConfig = {
   measurementId: import.meta.env.MEASUREMENTID,
 };
 export const app = initializeApp(firebaseConfig);
+
+/* functions to be used to authenticate */
+
+
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// const auth = getAuth();
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
+
+
+//   const auth = getAuth();
+// signOut(auth).then(() => {
+//   // Sign-out successful.
+// }).catch((error) => {
+//   // An error happened.
+// });
