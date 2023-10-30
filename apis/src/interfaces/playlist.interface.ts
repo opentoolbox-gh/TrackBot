482d5f9bc78196ce719a16508557b5da00d3ad28
pacifiquem <Murangwa Pacifique>
2023-10-30 11:46:22 +0200

    [docs] swagger documentation updates

--- apis/src/interfaces/playlist.interface.ts
+++ apis/src/interfaces/playlist.interface.ts
@@ -1,8 +1,16 @@
 import Video from "./video.interface";
+import mongoose from "mongoose";
 
 export default interface Playlist {
     name: String,
     thumbnail: String, 
     description: String, 
     videos: Video[]
+}
+
+export interface PlaylistMongooseSchema {
+    name: String,
+    thumbnail: String, 
+    description: String, 
+    videos: mongoose.Types.ObjectId[] 
 }
